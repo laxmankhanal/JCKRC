2d7073ed11a0f67b9a8de0c8911a718e60566fa6
laxmankhanal54 <laxmankhanal@lftechnology.com>
2017-04-22 20:14:30 +0545

    initial commit

---
+++ JCKRC/CellTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CellTableViewCell.swift
+//  JCKRC
+//
+//  Created by Pro on 4/22/17.
+//  Copyright © 2017 leapfrog-laxman. All rights reserved.
+//
+
+import UIKit
+
+class CellTableViewCell: UITableViewCell {
+
+  @IBOutlet var label: UILabel!
+
+}
